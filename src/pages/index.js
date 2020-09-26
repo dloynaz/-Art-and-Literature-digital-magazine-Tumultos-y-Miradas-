c862a42b0f28dfa6bb70eaf72a920d2dd33e3638
dloynaz <dloynaz@hotmail.com>
2020-09-25 18:59:38 -0600

    Some clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ const IndexPage = () => {
       <Head title="Home"/>
       <h1>Hello.</h1>
       <h2>I'm David, an almost full-stack developer</h2>
-      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
 
     </Layout>
   )
